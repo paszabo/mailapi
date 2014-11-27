cc860a8dc0505daefb3d93ca1cc576364f0e379f
Paul Szabo <paul@cupromotions.ca>
2014-11-27 11:46:53 -0500

    bumps version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
 
 setup(
     name='MailApi',
-    version='0.1.2',
+    version='0.1.3',
     description='An interface to iredadmin',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
