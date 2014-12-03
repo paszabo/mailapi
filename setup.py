1d078c0ecee7745ed7e54bf93d9acfcbd443dc56
Paul Szabo <paul@cupromotions.ca>
2014-12-03 10:27:29 -0500

    v0.1.7
    - Custom exception class definitions
        - They are mostly self-explanatory, but here is the break down:
            - AliasExists: raised when trying to create an alias that already exists in the database
            - DbInitError: raised when init_db has not been called
            - MailboxExists: raised when adding a mailbox that already exists in the database
            - NoSuchMailbox: raised when trying to access a mailbox that does not exist in the database
            - DomainExists: raised when trying to create a domain that already exists
            - NoSuchDomain: raised when trying to access a domain that does not exist in the database

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
 
 setup(
     name='MailApi',
-    version='0.1.6',
+    version='0.1.7',
     description='An interface to iredadmin',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
