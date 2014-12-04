5dc86a44ebe1efd760accb29245c2f2ab4012906
Paul Szabo <paul@cupromotions.ca>
2014-12-04 16:05:47 -0500

    v0.1.8
    - sorts mailboxes by username

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
 
 setup(
     name='MailApi',
-    version='0.1.7',
+    version='0.1.8',
     description='An interface to iredadmin',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
